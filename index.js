5d1d0e2e69ba99c0b039c49824bc07dd72e4d76a
Banks Humphreys <iobg0320@gmail.com>
2016-10-12 15:39:49 -0500

    finding file changes as it is recorded

--- index.js
+++ index.js
@@ -3,18 +3,27 @@
 var ScreenRecorder = require('screen-recorder').ScreenRecorder
 var path = require('path')
 var fs = require('fs')
- 
-var movie = new ScreenRecorder(path.resolve(__dirname, 'new.mp4')) // [, displayId]
-movie.setCapturesMouseClicks(true)
-movie.setCropRect(0, 0, 2560, 1600)
-movie.setFrameRate(30) // default is 15
-movie.recordAudio()
-movie.start()
 
-let read=fs.createReadStream('new.mp4')
-read.pipe(process.stdout)
+
+		var movie = new ScreenRecorder(path.resolve(__dirname, 'new.mp4')) // [, displayId]
+		movie.setCapturesMouseClicks(true)
+		movie.setCropRect(0, 0, 2560, 1600)
+		movie.setFrameRate(30) // default is 15
+		movie.recordAudio()
+		movie.start()
+
+
+
+let watch=fs.watchFile('new.mp4',(curr,prev)=>{
+	console.log(curr)
+})
+
+
+
+
 
 	
 setTimeout(function() {
   movie.stop()
+  fs.unlink('new.mp4')
 }, 10000)
