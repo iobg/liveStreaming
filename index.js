5e2bfba466989052b15338d8c86e96eaeac99dd8
Banks Humphreys <iobg0320@gmail.com>
2016-10-14 16:56:42 -0500

    finally found a good ffmpeg command, just need to figure out how to pipe it

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-const ffmpeg = require('fluent-ffmpeg')
-const command =ffmpeg()
-const fs = require('fs')
-var path = require('path')
-const exec = require('child_process').exec
-
-
-const read = fs.createReadStream('out.mpg')
-fs.watch('out.mpg', (eventType, filename) => {
-  if (filename) {
-    read.pipe(process.stdout)
-  } else {
-    console.log('file not found');
-  }
-});
-
+var ffmpeg = require('ffmpeg');
+var process = new ffmpeg('avfoundation')
+// could be useful
+// < /dev/null ffmpeg -f avfoundation -i "1" -preset ultrafast -tune zerolatency -f nut pipe:1
