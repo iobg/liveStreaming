f41c7464ca2ab44f14908ea32065fca57c61bfe1
Banks Humphreys <iobg0320@gmail.com>
2016-10-14 17:26:55 -0500

    found more useful commands

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var ffmpeg = require('ffmpeg');
 var process = new ffmpeg('avfoundation')
 // could be useful
-// < /dev/null ffmpeg -f avfoundation -i "1" -preset ultrafast -tune zerolatency -f nut pipe:1
+// < /dev/null ffmpeg -f avfoundation -i "1" -preset ultrafast -tune zerolatency -f avi pipe:1
+//< /dev/null ffmpeg -f avfoundation -i "1:0" -f avi pipe:1 higher quality
